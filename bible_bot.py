1c3c4c5a9ac809146fb699ff2dd9d0146c005c12
Paul <fifaplus@comcast.net>
2022-08-25 00:02:46 -0700

    Added words

--- bible_bot.py
+++ bible_bot.py
@@ -26,9 +26,9 @@ bot = commands.Bot(command_prefix=commands.when_mentioned_or("$"), intents=inten
 # variables
 base_url = "https://dailyverses.net/"
 swear_words = [
-    "ass", "bitch", "btch", "bullshit", "cock", "cum", "cunt", "damn", "dick", "dumbass", "fuck", "fucked", "fucking",
-    "fuc", "fuk", "nigga", "nigar", "niggar", "nigger", "penis", "piss", "pissant", "pissed", "pissing",
-    "pussy", "shit", "shitting", "turdface", "vagina", "vaginal"
+    "anal", "ass", "bitch", "btch", "bullshit", "cock", "cum", "cunt", "damn", "dick", "dumbass", "fuck",
+    "fucked", "fucking", "fuc", "fuk", "nigga", "nigar", "niggar", "nigger", "penis", "piss", "pissant",
+    "pissed", "pissing", "pussy", "shit", "shitting", "thot", "thots", "turdface", "vagina", "vaginal"
 ]
 
 
