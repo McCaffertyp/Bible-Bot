34b95419c811b6ebab26e542c4898b8463784e8f
Paul <fifaplus@comcast.net>
2022-08-26 16:02:44 -0700

    Added in hangman

--- bible_bot.py
+++ bible_bot.py
@@ -5,24 +5,19 @@ Created on Wed Aug 24 04:35:00 2022
 @author: Paul McCafferty
 @version: 1.0
 """
-import csv
-import operator
+
 # bot.py
 import os
+import csv
 import random
 import discord
-import threading
+import operator
 import http.client
 import html as html_helper
 from util import logger
 from discord.ext import commands
 from dotenv import load_dotenv
 from urllib.request import urlopen
-
-BIBLE_DICT_NAME = "Name"
-BIBLE_DICT_CHAPTERS = "Chapters"
-BIBLE_DICT_TOTAL_VERSES = "Total Verses"
-BIBLE_DICT_AVG_VERSES = "Average Verses"
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -38,14 +33,25 @@ else:
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix=commands.when_mentioned_or("$"), intents=intents)
 
-# variables
-votd_base_url = "https://dailyverses.net/"
-verse_lookup_base_url = "https://www.openbible.info/labs/cross-references/search?q="
-keyword_search_base_url = "https://www.biblegateway.com/quicksearch/?quicksearch="
+# Constants
+BIBLE_DICT_NAME = "Name"
+BIBLE_DICT_CHAPTERS = "Chapters"
+BIBLE_DICT_TOTAL_VERSES = "Total Verses"
+BIBLE_DICT_AVG_VERSES = "Average Verses"
+HANGMAN_DICT_SOLUTION = "solution"
+HANGMAN_DICT_PROGRESS = "progress"
+HANGMAN_DICT_MISTAKES_LEFT = "attempts"
+HANGMAN_DICT_GUESSES = "guesses"
+VOTD_BASE_URL = "https://dailyverses.net/"
+VERSE_LOOKUP_BASE_URL = "https://www.openbible.info/labs/cross-references/search?q="
+KEYWORD_SEARCH_BASE_URL = "https://www.biblegateway.com/quicksearch/?quicksearch="
+ENGLISH_ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+
+# Variables
 swear_words = [line.split("\n")[0] for line in open("swear_words.txt", "r").readlines()]
 operators = {"+": operator.add, "-": operator.sub, "*": operator.mul, "/": operator.truediv, "^": operator.pow}
-
-in_quiz = {}
+quizzing = {}
+playing_hangman = {}
 
 
 # Bot events
@@ -62,7 +68,7 @@ async def on_message(message):
     if message.guild.name == GUILD:
         await filter_message(message)
         await bot.process_commands(message)
-        if str(message.author) in in_quiz:
+        if str(message.author) in quizzing:
             await check_answer(message, message.content)
         elif "bro =" == message.content.lower():
             await send_message(message, "Wassup homie")
@@ -82,10 +88,13 @@ async def print_help(ctx):
     rlookup_help_text = "$rlookup\nLooks up and prints out a random verse."
     keyword_help_text = "$keyword [word]\nTakes in a singular keyword and searches online for the top related verse to print out as a response."
     quiz_help_text = "$quiz [option]\nSends a random verse with either the reference, a single word, or a sentence missing for the user to solve."
+    hangman_help_text = "$hangman [option]\nUsing hangman starts a game. Three modes and a status are the accepted arguments. Supports the following: easy, medium, hard, status"
+    hguess_help_text = "$hguess [guess]\nUsed to submit a guess to an ongoing hangman puzzle for the message sender."
     math_help_text = "$math [number_a] [operator] [number_b]\nProvided two numbers and a method of operation, the bot will produce the result. Supports the following: +, -, *, /"
-    help_text = "```{0}\n\n{1}\n\n{2}\n\n{3}\n\n{4}\n\n{5}\n\n{6}\n\n{7}```".format(
+    help_text = "```{0}\n\n{1}\n\n{2}\n\n{3}\n\n{4}\n\n{5}\n\n{6}\n\n{7}\n\n{8}\n\n{9}```".format(
         h_help_text, dailyvotd_help_text, votd_help_text, lookup_help_text,
-        rlookup_help_text, keyword_help_text, quiz_help_text, math_help_text
+        rlookup_help_text, keyword_help_text, quiz_help_text, hangman_help_text,
+        hguess_help_text, math_help_text
     )
     await send_message(ctx, help_text)
 
@@ -187,12 +196,12 @@ async def bible_quizzing(ctx, option: str = None):
 
     if option == "ref":
         quiz_verse = replace_characters(random_verse, verse_reference, "_")
-        in_quiz[quizzer] = verse_reference
+        quizzing[quizzer] = verse_reference
     elif option == "word":
         verse_words = verse_text.split(" ")
         remove_word = verse_words[random.randint(0, (len(verse_words) - 1))].replace(",", "")
         quiz_verse = replace_characters(random_verse, remove_word, "_")
-        in_quiz[quizzer] = remove_word
+        quizzing[quizzer] = remove_word
     else:
         verse_words = verse_text.split(" ")
         remove_word_count = random.randint(2, (len(verse_words) - 1))
@@ -202,15 +211,115 @@ async def bible_quizzing(ctx, option: str = None):
         remove_end_word = verse_words[remove_end_word_index]
         remove_sentence = random_verse[random_verse.find(remove_start_word):(random_verse.find(remove_end_word) + len(remove_end_word))]
         quiz_verse = replace_characters(random_verse, remove_sentence, "_")
-        in_quiz[quizzer] = remove_sentence
+        quizzing[quizzer] = remove_sentence
         # all_words = ""
         # for i in range(remove_start_index, (remove_start_index + remove_word_count)):
         #     remove_word = verse_words[i].replace(",", "")
         #     quiz_verse = replace_characters(quiz_verse, remove_word, "_")
         #     all_words = "{0},{1}".format(all_words, remove_word)
-        # in_quiz[quizzer] = all_words[1:(len(all_words) - 1)]
+        # quizzing[quizzer] = all_words[1:(len(all_words) - 1)]
 
     await send_message(ctx, "{0}, your quiz is:\n{1}".format(ctx.author.mention, quiz_verse))
+
+
+@bot.command(
+    name="hangman",
+    help="Using hangman starts a game. Three modes and a status are the accepted arguments.",
+    brief="Plays hangman with the user."
+)
+async def play_hangman(ctx, option: str = None):
+    if option is None:
+        await send_message(ctx, "I can't do anything for hangman with no option provided. Use $h for options.")
+    else:
+        if option != "easy" and option != "medium" and option != "hard" and option != "status":
+            await send_message(ctx, "The entered option \"{0}\" is unsupported.".format(option))
+            return
+
+        player = str(ctx.author)
+        player_mention = ctx.author.mention
+
+        if option == "status":
+            if player not in playing_hangman:
+                await send_message(ctx, "{0} you have no ongoing games! Start one with: $hangman [difficulty]".format(player_mention))
+                return
+            else:
+                current_progress = playing_hangman[player][HANGMAN_DICT_PROGRESS]
+                attempts_left = playing_hangman[player][HANGMAN_DICT_MISTAKES_LEFT]
+                status_update = "{0} - Progress:\n{1}\n\nRemaining Mistakes: {2}".format(player_mention, current_progress, attempts_left)
+                await send_message(ctx, handle_discord_formatting(status_update))
+                return
+
+        random_verse = get_random_verse()
+        verse_text_start = random_verse.find("\"") + 1
+        verse_text_end = random_verse.find("\"", verse_text_start)
+        verse_text = random_verse[verse_text_start:verse_text_end]
+
+        verse_reference_start_search = "\" - "
+        verse_reference_start = random_verse.find(verse_reference_start_search) + len(verse_reference_start_search)
+        verse_reference_end = random_verse.find(" ESV")
+        verse_reference = random_verse[verse_reference_start:verse_reference_end]
+
+        puzzle_answer = "\"{0}\" - {1}".format(verse_text, verse_reference)
+        puzzle_progress = create_hangman_puzzle(puzzle_answer)
+
+        playing_hangman[player] = dict()
+        playing_hangman[player][HANGMAN_DICT_SOLUTION] = puzzle_answer
+        playing_hangman[player][HANGMAN_DICT_PROGRESS] = puzzle_progress
+        if option == "easy":
+            playing_hangman[player][HANGMAN_DICT_MISTAKES_LEFT] = 10
+        elif option == "medium":
+            playing_hangman[player][HANGMAN_DICT_MISTAKES_LEFT] = 5
+        elif option == "hard":
+            playing_hangman[player][HANGMAN_DICT_MISTAKES_LEFT] = 3
+
+        puzzle_preview = "{0} - Here's your puzzle!\n{1}\n\nYou have {2} allowed Mistakes.".format(
+            player_mention, puzzle_progress, playing_hangman[player][HANGMAN_DICT_MISTAKES_LEFT]
+        )
+        await send_message(ctx, handle_discord_formatting(puzzle_preview))
+
+
+@bot.command(
+    name="hguess",
+    help="Used to submit a guess to an ongoing hangman puzzle for the message sender.",
+    brief="Submit a guess to ongoing hangman puzzle."
+)
+async def submit_hangman_guess(ctx, guess: str = None):
+    if guess is None:
+        await send_message(ctx, "No guess was supplied!")
+    elif guess.lower() not in ENGLISH_ALPHABET:
+        await send_message(ctx, "That guess is invalid!")
+    else:
+        player = str(ctx.author)
+        player_mention = ctx.author.mention
+        if player not in playing_hangman:
+            await send_message(ctx, "{0} you have no ongoing games! Start one with: $hangman [difficulty]".format(player_mention))
+            return
+
+        guess = guess.lower()
+        print(playing_hangman)
+        try:
+            previous_guesses = playing_hangman[player][HANGMAN_DICT_GUESSES]
+            if guess in previous_guesses:
+                await send_message(ctx, "{0} you have already guessed that letter! Try a different one.".format(player_mention))
+                return
+        except KeyError as error:
+            previous_guesses = ""
+            logger.w("Tried to access a non-existent key")
+
+        puzzle_solution = playing_hangman[player][HANGMAN_DICT_SOLUTION]
+        if guess in puzzle_solution:
+            puzzle_progress = playing_hangman[player][HANGMAN_DICT_PROGRESS]
+            updated_progress = update_hangman_puzzle_progress(guess, puzzle_solution, puzzle_progress)
+            playing_hangman[player][HANGMAN_DICT_PROGRESS] = updated_progress
+        else:
+            attempts_remaining = int(playing_hangman[player][HANGMAN_DICT_MISTAKES_LEFT])
+            playing_hangman[player][HANGMAN_DICT_MISTAKES_LEFT] = attempts_remaining - 1
+
+        playing_hangman[player][HANGMAN_DICT_GUESSES] = "{0}{1}".format(previous_guesses, guess)
+        current_progress = playing_hangman[player][HANGMAN_DICT_PROGRESS]
+        attempts_left = playing_hangman[player][HANGMAN_DICT_MISTAKES_LEFT]
+        status_update = "{0} - Progress:\n{1}\n\nRemaining Mistakes: {2}".format(player_mention, current_progress, attempts_left)
+        await send_message(ctx, handle_discord_formatting(status_update))
 
 
 @bot.command(
@@ -253,12 +362,12 @@ async def check_answer(message, answer: str):
         return
     quizzer = str(message.author)
     username = quizzer.split("#")[0]
-    correct_answer: str = in_quiz[quizzer]
+    correct_answer: str = quizzing[quizzer]
     if answer.lower() != correct_answer.lower():
         await send_message(message, "Sorry, {0}, that is incorrect. Was looking for:\n{1}".format(username, correct_answer))
     else:
         await send_message(message, "Congratulations, {0}, that is correct!".format(username))
-    in_quiz.__delitem__(quizzer)
+    quizzing.__delitem__(quizzer)
 
 
 def convert_twenty_four_to_twelve(time: str) -> str:
@@ -277,8 +386,18 @@ def convert_twenty_four_to_twelve(time: str) -> str:
         return "{0}:{1} PM".format(23, minute)
 
 
+def create_hangman_puzzle(verse: str) -> str:
+    puzzle = ""
+    for c in verse:
+        if c.lower() in ENGLISH_ALPHABET:
+            puzzle = "{0}_".format(puzzle)
+        else:
+            puzzle = "{0}{1}".format(puzzle, c)
+    return puzzle
+
+
 def get_votd_from_url() -> str:
-    lookup_url = "{0}{1}".format(votd_base_url, "esv")
+    lookup_url = "{0}{1}".format(VOTD_BASE_URL, "esv")
     webpage: http.client.HTTPResponse = urlopen(lookup_url)
     html_bytes: bytes = webpage.read()
     html: str = html_helper.unescape(html_bytes.decode("utf-8"))
@@ -303,9 +422,9 @@ def get_verse_from_lookup_url(book: str, chapter_verse: str, book_num: str = Non
     verse = chapter_verse_split[1]
     logger.d("Looking up: book_num={0}, book={1}, chapter={2}, verse={3}".format(book_num, book, chapter, verse))
     if book_num is None:
-        lookup_url = "{0}{1}+{2}%3A{3}".format(verse_lookup_base_url, book.replace(" ", "+"), chapter, verse)
+        lookup_url = "{0}{1}+{2}%3A{3}".format(VERSE_LOOKUP_BASE_URL, book.replace(" ", "+"), chapter, verse)
     else:
-        lookup_url = "{0}{1}+{2}+{3}%3A{4}".format(verse_lookup_base_url, book_num, book.replace(" ", "+"), chapter, verse)
+        lookup_url = "{0}{1}+{2}+{3}%3A{4}".format(VERSE_LOOKUP_BASE_URL, book_num, book.replace(" ", "+"), chapter, verse)
 
     logger.d("Using url={0}".format(lookup_url))
     webpage: http.client.HTTPResponse = urlopen(lookup_url)
@@ -328,7 +447,7 @@ def get_verse_from_lookup_url(book: str, chapter_verse: str, book_num: str = Non
 
 
 def get_verse_from_keyword_url(word) -> str:
-    lookup_url = "{0}{1}&version=ESV".format(keyword_search_base_url, word)
+    lookup_url = "{0}{1}&version=ESV".format(KEYWORD_SEARCH_BASE_URL, word)
     webpage: http.client.HTTPResponse = urlopen(lookup_url)
     html_bytes: bytes = webpage.read()
     html: str = html_helper.unescape(html_bytes.decode("utf-8"))
@@ -405,6 +524,17 @@ def get_random_verse() -> str:
         return remove_html_tags(verse_text)
 
 
+def handle_discord_formatting(text: str) -> str:
+    discord_formatted = ""
+    for c in text:
+        if c == "_" or c == "*":
+            discord_formatted = "{0}\\{1}".format(discord_formatted, c)
+        else:
+            discord_formatted = "{0}{1}".format(discord_formatted, c)
+
+    return discord_formatted
+
+
 def is_banned_word(swear_word: str, sentence: str) -> bool:
     start_index_of_swear_word = sentence.find(swear_word)
     end_index_of_swear_word = start_index_of_swear_word + len(swear_word)
@@ -440,6 +570,20 @@ def replace_characters(s: str, c: str, r: str) -> str:
     for i in range(0, len(c), len(r)):
         full_replace_string = "{0}\\{1}".format(full_replace_string, r)
     return s.replace(c, full_replace_string)
+
+
+def update_hangman_puzzle_progress(guess: str, solution: str, progress: str) -> str:
+    solution_characters = [c for c in solution]
+    progress_characters = [c for c in progress]
+    updated_puzzle = ""
+
+    for i in range(0, len(solution_characters)):
+        if solution_characters[i] == guess:
+            updated_puzzle = "{0}{1}".format(updated_puzzle, guess)
+        else:
+            updated_puzzle = "{0}{1}".format(updated_puzzle, progress_characters[i])
+
+    return updated_puzzle
 
 
 bot.run(TOKEN)
--- util/time.py
+++ util/time.py
@@ -7,7 +7,13 @@ def get_current_datetime() -> str:
     month = time_now.month
     day = time_now.day
     hour = time_now.hour
+    if int(hour) < 10:
+        hour = "0{0}".format(hour)
     minute = time_now.minute
+    if int(minute) < 10:
+        minute = "0{0}".format(minute)
     second = time_now.second
+    if int(second) < 10:
+        second = "0{0}".format(second)
 
     return "{0}-{1}-{2} {3}:{4}:{5}".format(year, month, day, hour, minute, second)
